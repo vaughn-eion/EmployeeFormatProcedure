c0d03be577d6a78c40e33ab9020c20f07aa40ab1
vaughn-eion <108884474+vaughn-eion@users.noreply.github.com>
2022-07-07 10:49:11 -0400

    Create EmployeeFormatProcedure.sql

---
+++ EmployeeFormatProcedure.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE FormEmployeeData as 
+begin
+    delete from [dbo].[Employees];
+    with TempEmployee as (
+        select UPPER(LEFT(concat(ISNULL(Name, ''), ISNULL(FirstName, '')),1))+LOWER(SUBSTRING(concat(ISNULL(Name, ''), ISNULL(FirstName, '')),2,LEN(concat(ISNULL(Name, ''), ISNULL(FirstName, ''))))) as Name, PhoneNumber, left(ZipCode, 5) as ZipCode, Date from EmployeesJsonData
+    ),
+    TempEmployeeJoined as (
+        select Name, max(PhoneNumber) as PhoneNumber, max(ZipCode) as ZipCode, max(Date) as Date from TempEmployee group by Name
+    )
+    Insert into Employees (ID, Name, PhoneNumber, EmailAddress, ZipCode, State, Date)
+    select newID() as ID, t.Name, format(t.PhoneNumber, '(000)-000-0000'), concat(lower(Name), '@p3adaptive.com') as EmailAddress, t.ZipCode, z.State, DATEADD(S, CONVERT(int,LEFT(t.Date, 10)), '1970-01-01') as Date from TempEmployeeJoined t 
+    left join ZipCodeData z on t.ZipCode = z.ZipCode
+end
