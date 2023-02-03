cc7c18a3d06d3ce19118a83a502a769c58a728b3
vaughn-eion <108884474+vaughn-eion@users.noreply.github.com>
2023-02-03 17:08:37 -0500

    Update EmployeeFormatProcedure.sql

--- EmployeeFormatProcedure.sql
+++ EmployeeFormatProcedure.sql
@@ -8,6 +8,6 @@ begin
         select Name, max(PhoneNumber) as PhoneNumber, max(ZipCode) as ZipCode, max(Date) as Date from TempEmployee group by Name
     )
     Insert into Employees (ID, Name, PhoneNumber, EmailAddress, ZipCode, State, Date)
-    select newID() as ID, t.Name, format(t.PhoneNumber, '(000)-000-0000'), concat(lower(Name), '@p3adaptive.com') as EmailAddress, t.ZipCode, z.State, DATEADD(S, CONVERT(int,LEFT(t.Date, 10)), '1970-01-01') as Date from TempEmployeeJoined t 
+    select newID() as ID, t.Name, format(t.PhoneNumber, '(000)-000-0000'), concat(lower(Name), 'genericdomain.com') as EmailAddress, t.ZipCode, z.State, DATEADD(S, CONVERT(int,LEFT(t.Date, 10)), '1970-01-01') as Date from TempEmployeeJoined t 
     left join ZipCodeData z on t.ZipCode = z.ZipCode
 end
